ebf017ee10a138b26b9a170255b7588bb5091d5b
Joseph Scholl <composerofsoftware@gmail.com>
2025-10-18 03:05:43 -0600

    Use persistent Chrome profile to avoid repeated logins. I got this working

--- scraping/get_sylabus_for_class.py
+++ scraping/get_sylabus_for_class.py
@@ -1,4 +1,5 @@
 from time import sleep
+import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,27 +9,32 @@ import chromedriver_autoinstaller
 
 chromedriver_autoinstaller.install()
 
+# if we provide these chrome options, we can use the same browser profile and avoid having to log in every time we run the script
 chrome_options = Options()
-chrome_options.add_argument(
-    "user-data-dir=./private_browser_profile"
-)  # This directory will store the profile
+chrome_options.add_argument("user-data-dir=./.private_browser_data")
+chrome_options.add_argument("profile-directory=Default")
 driver = webdriver.Chrome(options=chrome_options)
 
-
-driver = webdriver.Chrome()
-
-
 web = "https://syllabus.byu.edu"
+
 driver.get(web)
 
-# Example wait for an element to be visible (adjust as needed)
-element_username = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.ID, "username"))
-)
-element_password = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.ID, "password"))
-)
+sleep(5)
 
-WebDriverWait(driver, 300).until(EC.url_matches("https://syllabus.byu.edu"))
+try:
+    # if we were redirected to the login page, we need to wait for the user to log in manually
+    element_username = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.ID, "username"))
+    )
+    element_password = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.ID, "password"))
+    )
+    WebDriverWait(driver, 300).until(EC.url_matches("https://syllabus.byu.edu"))
+except:
+    # ignore the exception that would be thrown when we are already logged in and immediately load the syllabus page
+    pass
 
-sleep(30)
+WebDriverWait(driver, 10).until(EC.url_matches("https://syllabus.byu.edu"))
+sleep(2)
+
+sleep(30000)
