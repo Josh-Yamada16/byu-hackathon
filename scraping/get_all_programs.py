c254f53c92fb1560755bb84e774717e420b6985b
Joseph Scholl <composerofsoftware@gmail.com>
2025-10-17 23:29:27 -0600

    updated programs.json file. commited the python script I used to get it all

---
+++ scraping/get_all_programs.py
@@ -0,0 +1,113 @@
+import requests
+import json  # Ensure the json module is imported
+
+url = "https://app.coursedog.com/api/v1/cm/byu/programs/search/$filters?limit=312"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:142.0) Gecko/20100101 Firefox/142.0",
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Content-Type": "application/json",
+    "Referer": "https://catalog.byu.edu/",
+    "X-Requested-With": "catalog",
+    "Origin": "https://catalog.byu.edu",
+    "Sec-GPC": "1",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "cross-site",
+    "Connection": "keep-alive",
+    "TE": "trailers",
+}
+data = {
+    "condition": "AND",
+    "filters": [
+        {
+            "filters": [
+                {
+                    "id": "level-program",
+                    "condition": "field",
+                    "name": "level",
+                    "inputType": "select",
+                    "group": "program",
+                    "type": "isNot",
+                    "value": "Doctoral",
+                },
+                {
+                    "id": "level-program",
+                    "condition": "field",
+                    "name": "level",
+                    "inputType": "select",
+                    "group": "program",
+                    "type": "isNot",
+                    "value": "Graduate Minor",
+                },
+                {
+                    "id": "level-program",
+                    "condition": "field",
+                    "name": "level",
+                    "inputType": "select",
+                    "group": "program",
+                    "type": "isNot",
+                    "value": "Masters",
+                },
+                {
+                    "id": "degreeDesignation-program",
+                    "condition": "field",
+                    "name": "degreeDesignation",
+                    "inputType": "select",
+                    "group": "program",
+                    "type": "isNot",
+                    "value": "PBS",
+                },
+                {
+                    "id": "code-program",
+                    "condition": "field",
+                    "name": "code",
+                    "inputType": "text",
+                    "group": "program",
+                    "type": "isNot",
+                    "value": "322619",
+                },
+                {
+                    "id": "degreeDesignation-program",
+                    "condition": "field",
+                    "name": "degreeDesignation",
+                    "inputType": "select",
+                    "group": "program",
+                    "type": "isNot",
+                    "value": "PRE",
+                },
+                {
+                    "id": "level-program",
+                    "condition": "field",
+                    "name": "level",
+                    "inputType": "select",
+                    "group": "program",
+                    "type": "isNot",
+                    "value": "Non Degree",
+                },
+                {
+                    "id": "catalogDisplayName-program",
+                    "condition": "field",
+                    "name": "catalogDisplayName",
+                    "inputType": "text",
+                    "group": "program",
+                    "type": "contains",
+                    "value": "(",
+                },
+            ],
+            "id": "kFsSkKHp",
+            "condition": "and",
+        }
+    ],
+}
+
+response = requests.post(url, headers=headers, json=data)
+
+if response.status_code == 200:
+    programs = response.json()  # Get the response data
+    with open("programs.json", "w") as f:
+        json.dump(programs, f, indent=4)  # Save the response to programs.json
+else:
+    print(f"Error: {response.status_code}, {response.text}")
+    print(f"Error: {response.status_code}, {response.text}")
